1536049bd4b244a8a4be0f00a224624c9789f363
Hummelgaard89 <casper.hummelgaard@live.dk>
2021-02-22 12:15:37 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    public class Square
+    {
+        private int a;
+        private int perimeter;
+        private int area;
+
+        public Square()
+        {
+            a = 4;
+        }
+        public void Calculate_Square_Perimeter() //Metode til udregning af omkreds
+        {
+            perimeter = a * 4;
+        }
+        public void Calculate_Square_Area() //Metode til udregning af areal
+        {
+            area = a * 2;
+        }
+        public int SideA //get/set af side a.
+        {
+            get
+            {
+                return this.a;
+            }
+            set
+            {
+                this.a = value;
+            }
+        }
+        public void Print_Square() //Metode til at udskrive firkant
+        {
+            Console.WriteLine("Side a is = " + a);
+            Console.WriteLine("Perimeter of the square is = " + perimeter);
+            Console.WriteLine("Area of the square is = " + area);
+        }
+    }
+}
---
+++ Square Class.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Her Opretter jeg første firkant
+            Square FirkantA = new Square();
+            FirkantA.Calculate_Square_Perimeter();
+            FirkantA.Calculate_Square_Area();
+            FirkantA.Print_Square();
+            Console.WriteLine();
+            ///////////////////////////////////////
+            //Her opretter jeg anden firkant og ændre værdien med get/set
+            Square FirkantB = new Square();
+            FirkantB.SideA = 6;
+            FirkantB.Calculate_Square_Perimeter();
+            FirkantB.Calculate_Square_Area();
+            FirkantB.Print_Square();
+            Console.WriteLine();
+            ///////////////////////////////////////
+            //Her opretter jeg 3 firkant og ændre værdien med get/set
+            Square FirkantC = new Square();
+            FirkantC.SideA = 8;
+            FirkantC.Calculate_Square_Perimeter();
+            FirkantC.Calculate_Square_Area();
+            FirkantC.Print_Square();
+            Console.ReadLine();
+        }
+    }
+}
